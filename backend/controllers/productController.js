e165591739199371383b6cccc598b5e1b3c9df71
hurairarazzak <hurairarazzak125@gmail.com>
2025-01-15 00:52:45 +0500

    listProducts, removeProduct, singleProduct API are created.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -52,24 +52,35 @@ const addProduct = async (req, res) => {
 // Function for list product
 const listProducts = async (req, res) => {
     try {
-
-        const products = productModel.find({})
+        const products = await productModel.find({})
         res.json({ success: true, products })
-
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message })
     }
-
 }
 
 // Function for removing product
 const removeProduct = async (req, res) => {
-
+    try {
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({ success: true, message: "Product Removed" })
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 }
 
 // Function for single product info
 const singleProduct = async (req, res) => {
+    try {
+        const {productId} = req.body
+        const product = await productModel.findById(productId)
+        res.json({success: true, product})
+    } catch (error) {
+        console.log(error);
+        res.json({success: true, message: error.message })
+    }
 
 }
 
