46d4611f764d40d5db5fe21c60e752c10c43b3b7
hurairarazzak <hurairarazzak125@gmail.com>
2025-01-15 09:09:48 +0500

    Basic pages and images created.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <img className='w-[max(10%, 80px)]' src={assets.logo} alt="" />
+      <button className='bg-gray-600 px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
   )
 }
 
