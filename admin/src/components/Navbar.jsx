13b8e591a89122e8b7fee56eeb1d0bc30d2e5783
hurairarazzak <hurairarazzak125@gmail.com>
2025-01-16 23:43:00 +0500

    Sidebar, Navbar and Getting Login Api data added.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div>
-      <img className='w-[max(10%, 80px)]' src={assets.logo} alt="" />
-      <button className='bg-gray-600 px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    <div className='flex items-center py-6 px-[4%] justify-between'>
+      <img className='w-[11em]' src={assets.logo} alt="" />
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
