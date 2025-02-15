e08ad596137568a29409eefad50d499f8a8eab79
hurairarazzak <hurairarazzak125@gmail.com>
2025-02-15 22:10:38 +0500

    orderController API function base created.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,13 +1,33 @@
-import React from 'react'
 
-const orderController = () => {
 
-  return (
-    <div>
-      <h1>orderController</h1>
-
-    </div>
-  )
+// Placing orders using COD
+const placeOrder = async (req, res) => {
+  
 }
 
-export default orderController
+// Placing orders using Stripe Method
+const placeOrderStripe = async (req, res) => {
+  
+}
+
+// Placing orders using Easypaisa OR Bank Account Method  
+const placeOrderEasypaisa = async (req, res) => {
+  
+}
+
+// All Orders data for Admin Panel
+const allOrders = async (req, res) => {
+  
+}
+
+// User Order Data for Frontend 
+const userOrders = async (req, res) => {
+  
+}
+
+// Update Order Status from Admin Panel 
+const updateStatus = async (req, res) => {
+  
+}
+
+export { placeOrder, placeOrderStripe, placeOrderEasypaisa, allOrders, userOrders, updateStatus }
