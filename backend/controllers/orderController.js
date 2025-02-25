585f0a93f38ed7c89861d6a3665a333b1be9c186
hurairarazzak <hurairarazzak125@gmail.com>
2025-02-25 11:50:56 -0800

    Displayed only User Ordered data on there order page.

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -46,7 +46,20 @@ const allOrders = async (req, res) => {
 
 // User Order Data for Frontend 
 const userOrders = async (req, res) => {
-  
+  try {
+    
+    const { userId }  = req.body
+
+    const orders = await orderModel.find({ userId })
+    res.json({success: true, orders})
+
+  } catch (error) {
+
+    console.log(error);
+    res.json({ success: false, message: error.message });
+    
+  }
+
 }
 
 // Update Order Status from Admin Panel
--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -7,7 +7,7 @@ import { assets } from "../assets/assets";
 import { ShopContext } from "../context/ShopContext";
 
 const PlaceOrder = () => {
-  const [method, setMethod] = useState("cod");
+  const [method, setMethod] = useState("COD");
   const { navigate, backendUrl, token, cartItems, setCartItems, getCartAmount, delivery_fee, products } = useContext(ShopContext); // Fix here
   const [formData, setFormData] = useState({
     firstName: "",
@@ -169,16 +169,16 @@ const PlaceOrder = () => {
           <div className="flex gap-3 flex-col lg:flex-row">
             {/* ------------- Payment Method Selection ------------ */}
             <div className="flex gap-3 flex-col lg:flex-row">
-              <div onClick={()=>setMethod('stripe')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-green-400' : ''}`}></p>
+              <div onClick={()=>setMethod('STRIPE')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'STRIPE' ? 'bg-green-400' : ''}`}></p>
                 <img className="h-5 mx-4" src={assets.stripe_icon} alt="" />
               </div>
-              <div onClick={()=>setMethod('paypal')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'paypal' ? 'bg-green-400' : ''}`}></p>
+              <div onClick={()=>setMethod('PAYPAL')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'PAYPAL' ? 'bg-green-400' : ''}`}></p>
                 <img className="h-5 mx-4" src={assets.paypal_icon} alt="" />
               </div>
-              <div onClick={()=>setMethod('cod')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-green-400' : ''}`}></p>
+              <div onClick={()=>setMethod('COD')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+                <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'COD' ? 'bg-green-400' : ''}`}></p>
                 <p className="text-gray-500 text-sm font-medium mx-4">CASH ON DELIVERY</p>
               </div>
             </div>
